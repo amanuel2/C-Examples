4e361048c94d9c8b625edb9f6a9cec87b09ddb99
Amanuel <amanuelb43@gmail.com>
2016-04-22 16:35:02 -0400

    Update main2.cpp

--- Inheritance/Polymorhism/Virtual-Functions/main2.cpp
+++ Inheritance/Polymorhism/Virtual-Functions/main2.cpp
@@ -1,214 +1,141 @@
-#include <iostream>
-#include <string>
+#
+include < iostream > #include < string >
 
-class Character
-{
+    class Character {
 
+        public:
+            Character(std::string name_, double ID_, double intelegience_, double speed_, double strength_) {
 
-  public:
-    Character(std::string name_, double ID_, double intelegience_, double
-      speed_, double strength_)
-    {
+                name = name_;
 
+                ID = ID_;
 
-        name = name_;
+                intelegience = intelegience_;
 
+                speed = speed_;
 
-        ID = ID_;
+                strength = strength_;
 
+            }
 
-        intelegience = intelegience_;
+        void setID(double ID_) {
 
+            ID = ID_;
 
-        speed = speed_;
+        }
 
+        void setIntelegience(double intelegience_) {
 
-        strength = strength_;
+            intelegience = intelegience_;
 
+        }
 
+        void setSpeed(double speed_) {
 
-    }
+            speed = speed_;
 
-    void setID(double ID_)
-    {
+        }
 
-        ID = ID_;
+        void setStrength(double strength_) {
 
+            strength = strength_;
 
-    }
+        }
 
-    void setIntelegience(double intelegience_)
-    {
+        void setName(std::string name_) {
 
-        intelegience = intelegience_;
+            name = name_;
 
+        }
 
-    }
+        double getID() {
 
-    void setSpeed(double speed_)
-    {
+            return ID;
 
-        speed = speed_;
+        }
 
+        double getIntellegence() {
 
-    }
+            return intelegience;
 
-    void setStrength(double strength_)
-    {
+        }
 
-        strength = strength_;
+        double getSpeed() {
 
+            return speed;
 
-    }
+        }
 
-    void setName(std::string name_)
-    {
+        double getStrength() {
 
-        name = name_;
+            return strength;
 
+        }
 
-    }
+        std::string getName() {
 
-    double getID()
-    {
+            return name;
 
-        return ID;
+        }
 
+        virtual std::string startNoise() = 0;
 
-    }
+        protected:
+            std::string name;
 
+        double ID;
 
-    double getIntellegence()
-    {
+        double intelegience;
 
-        return intelegience;
+        double speed;
 
-
-    }
-
-
-    double getSpeed()
-    {
-
-        return speed;
-
+        double strength;
 
     }
-
-
-    double getStrength()
-    {
-
-        return strength;
-
-
-    }
-
-
-    std::string getName()
-    {
-
-        return name;
-
-
-    }
-
-
-    virtual std::string startNoise() = 0;
-
-
-  protected:
-    std::string name;
-
-
-    double ID;
-
-
-    double intelegience;
-
-
-    double speed;
-
-
-    double strength;
-
-
-
-
-}
 
 ;
 
+class Hero: public Character {
 
-
-class Hero: public Character
-{
-
-
-  public:
-    Hero(std::string name_, double ID_, double intelegience_, double speed_,
-      double strength_, int livesSaved_): Character(name_, ID_, intelegience_,
-      speed_, strength_)
-    {
-
+    public: Hero(std::string name_, double ID_, double intelegience_, double speed_,
+        double strength_, int livesSaved_): Character(name_, ID_, intelegience_,
+        speed_, strength_) {
 
         livesSaved = livesSaved_;
 
-
-
     }
 
-    int getLivesSaved()
-    {
+        int getLivesSaved() {
 
         return livesSaved;
 
-
     }
 
-
-    void setLivesSaved(int livesSaved_)
-    {
+        void setLivesSaved(int livesSaved_) {
 
         livesSaved = livesSaved_;
 
-
     }
 
-    virtual std::string startNoise()
-    {
-
+        virtual std::string startNoise() {
 
         return "May the realm of my Victory enclose you!";
 
-
-
     }
 
-
-  protected:
-    int livesSaved;
-
-
+        protected: int livesSaved;
 
 }
 
 ;
 
+class Enemy: public Character {
 
+    public: Enemy(std::string name_, double ID_, double intelegience_, double speed_,
+        double strength_, int heroesStopped_,
 
-class Enemy: public Character
-{
-
-
-  public:
-    Enemy(std::string name_, double ID_, double intelegience_, double speed_,
-      double strength_, int heroesStopped_, 
-
-    std::string type_, bool mainiac_): Character(name_, ID_, intelegience_,
-      speed_, strength_)
-    {
-
+        std::string type_, bool mainiac_): Character(name_, ID_, intelegience_,
+        speed_, strength_) {
 
         heroesStopped_ = heroesStopped_;
 
@@ -216,139 +143,91 @@ class Enemy: public Character
 
         mainac = mainiac_;
 
-
-
     }
 
-    void setHeroesStopped(int heroesStopped_)
-    {
+        void setHeroesStopped(int heroesStopped_) {
 
         heroesStopped = heroesStopped_;
 
-
     }
 
-    void setType(std::string type_)
-    {
+        void setType(std::string type_) {
 
         type = type_;
 
-
     }
 
-    void setMainac(bool mainac_)
-    {
+        void setMainac(bool mainac_) {
 
         mainac = mainac_;
 
-
     }
 
-    int getHeroesStopped()
-    {
+        int getHeroesStopped() {
 
         return heroesStopped;
 
-
     }
 
-
-    std::string getType()
-    {
+        std::string getType() {
 
         return type;
 
-
     }
 
-
-    bool getMainac()
-    {
+        bool getMainac() {
 
         return mainac;
 
-
     }
 
-
-    virtual std::string startNoise()
-    {
-
+        virtual std::string startNoise() {
 
         return "I Will ALWAYS CONQUER! Lie Beneath ME!";
 
-
-
     }
 
-
-  protected:
-    int heroesStopped;
-
+        protected: int heroesStopped;
 
     std::string type;
 
-
     bool mainac;
-
-
 
 }
 
 ;
 
+int main() {
 
+    Hero Amanuel("Amanuel Bogale", 132, 97.32, 59.2, 77.33, 21);
 
+    Enemy Sens("Sens Cueva", 21, 77.43, 95.32, 83.23, 11, "MAINIAC", true);
 
-int main()
-{
+    Amanuel.setID(111);
 
+    // Sens Upgrades his intellegience
+    Sens.setIntelegience(88.2);
 
-  Hero Amanuel("Amanuel Bogale", 132, 97.32, 59.2, 77.33, 21);
+    // Amanuel Needs to work on speed...
+    Amanuel.setSpeed(70.21);
 
+    // Print Objects..
+    Character * humans[2] = {
 
-  Enemy Sens("Sens Cueva", 21, 77.43, 95.32, 83.23, 11, "MAINIAC", true);
+        & Amanuel,
+        & Sens
 
+    };
 
+    for (int i = 0; i < 2; i++) {
 
-  Amanuel.setID(111);
+        std::cout << "*** " << humans[i] - > getName() << " ***" << std::endl <<
+            "INTELLIEGENCE : " << humans[i] - > getIntellegence() << std::endl <<
+            "SPEED : " << humans[i] - > getSpeed() << std::endl << "ID : " << humans[i] - > getID() << std::endl << "STRENGTH : " << humans[i] - > getStrength() <<
+            std::endl << "START NOISE : " << humans[i] - > startNoise() << std::endl;
 
+    }
 
-  // Sens Upgrades his intellegience
-  Sens.setIntelegience(88.2);
-
-
-  // Amanuel Needs to work on speed...
-  Amanuel.setSpeed(70.21);
-
-
-  // Print Objects..
-  Character *humans[2] = 
-  {
-
-
-     &Amanuel,  &Sens 
-
-  }
-  ;
-
-
-  for (int i = 0; i < 2; i++)
-  {
-
-
-    std::cout << "*** " << humans[i]->getName() << " ***" << std::endl << 
-      "INTELLIEGENCE : " << humans[i]->getIntellegence() << std::endl << 
-      "SPEED : " << humans[i]->getSpeed() << std::endl << "ID : " << humans[i]
-      ->getID() << std::endl << "STRENGTH : " << humans[i]->getStrength() <<
-      std::endl << "START NOISE : " << humans[i]->startNoise() << std::endl;
-
-
-
-  }
-
-  return 0;
-
-
+    return 0;
 
 }
