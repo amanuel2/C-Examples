c819fb3be3da86d25eaf4cec11ee5ae9d09bea91
Amanuel <amanuelb43@gmail.com>
2016-04-26 08:29:18 -0400

    Update main.cpp

--- Comparison/PassByRefrence-Vs-PassByPointer/main.cpp
+++ Comparison/PassByRefrence-Vs-PassByPointer/main.cpp
@@ -1,35 +1,35 @@
 #include<iostream>
 #include<string>
 
-void passByAdress(int *a);
-void passByRefrence(int &a);
+void passByAddress(int *a);
+void passByReference(int &a);
 
 int main() {
-	int betty = 21;
-	int setty = 45;
+    int betty = 21;
+    int setty = 45;
 
-	passByAdress(&betty);
-	passByRefrence(setty);
+    passByAddress(&betty);
+    passByReference(setty);
 
-	std::cout << "Betty : " << betty << std::endl;
-	std::cout << "Setty : " << setty << std::endl;
+    std::cout << "Betty : " << betty << std::endl;
+    std::cout << "Setty : " << setty << std::endl;
 }
 
 //Pass By Adress
-void passByAdress(int *a) {
-	*a = *a + 5;
-	//Memory Adress of a.
-	//So gives hexa decimal.
-	std::cout << "Address : " << a << std::endl;
-	//Actual Value of adress
-	std::cout << "Adress's Value : " << *a << std::endl;
+void passByAdrress(int *a) {
+    *a = *a + 5;
+    //Memory Adress of a.
+    //So gives hexa decimal.
+    std::cout << "Address : " << a << std::endl;
+    //Actual Value of adress
+    std::cout << "Address's Value : " << *a << std::endl;
 }
-//Pass By Refrence
-void passByRefrence(int &a) {
-	a = a + 5;
-	//Memory Adress of a.
-	//So gives hexa decimal.
-	std::cout << "Adress : " << &a << std::endl;
-	//Actual Value of adress
-	std::cout << "Adress's Value " << a << std::endl;
+//Pass By Reference
+void passByReference(int &a) {
+    a = a + 5;
+    //Memory Address of a.
+    //So gives hexa decimal.
+    std::cout << "Adrress : " << &a << std::endl;
+    //Actual Value of address
+    std::cout << "Address's Value " << a << std::endl;
 }
