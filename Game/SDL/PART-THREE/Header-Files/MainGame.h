6aab45b599269a797fd9dac37834b0804af4f6af
Amanuel <amanuelb43@gmail.com>
2016-04-29 16:07:28 -0400

    Create MainGame.h

---
+++ Game/SDL/PART-THREE/Header-Files/MainGame.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<Windows.h>
+#include<SDL.h>
+#include<GL/glew.h>
+#include<string>
+#include <algorithm>
+#include<iostream>
+
+
+enum class GameState {
+	PLAY,
+	EXIT
+};
+
+class MainGame
+{
+public:
+	MainGame(int width, int height);
+	~MainGame();
+
+	void run();
+
+private:
+	void initSystem();
+	void processInput();
+	void gameLoop();
+	void quitCheck();
+	void drawGame();
+
+	int _screenWidth;
+	int _screenHieght;
+	GameState __gameState;
+	SDL_Window *__window;
+};
+
