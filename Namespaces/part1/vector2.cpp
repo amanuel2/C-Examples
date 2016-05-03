78a38559bdda5edb01286cac54e56c2f76c5bdd5
Amanuel <amanuelb43@gmail.com>
2016-05-03 17:30:21 -0400

    Create vector2.cpp

---
+++ Namespaces/part1/vector2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include <stdio.h>      /* printf, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+
+
+namespace vectorTwo {
+
+	int randNumber(int *rChange, int min, int max) {
+		srand(time(NULL));
+		int poll = rand() % (max + 1) + min; 
+		*rChange = poll;
+		return 0;
+	}
+	int _X, _Y;
+	void setNumbers() {
+		_X = randNumber(&_X, 10, 20);
+		_Y = randNumber(&_Y, 10, 20);
+	}
+}
