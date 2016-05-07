8289d1d264b23760bf8d8c849b51c5369ed5ebe1
Amanuel <amanuelb43@gmail.com>
2016-05-07 18:15:58 -0400

    Create main.cpp

---
+++ Preprocessor-Directives/if-elseif-else/pt1/main.cpp
@@ -0,0 +1,25 @@
+/*
+Simple Ilustration of #if #elif and #else
+Author : Amanuel Bogale
+*/
+
+#include<iostream>
+
+#define WINDOWS 1
+#define LINUX 0
+#define MAC 1
+
+#define OS Windows
+int main(){
+
+#if OS == Windows
+ std::cout << "OS IS WINDOWS";
+
+#elif OS == LINUX
+   std::cout << "LINUX";
+ #else
+   std::cout << "SDSD";
+
+#endif // OS
+std::cout << "DFS" << std::endl;
+}
