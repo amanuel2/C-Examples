cd523271a0bc812cc2be29e5a795b4a028f61ab4
Amanuel <amanuelb43@gmail.com>
2016-05-09 19:32:15 -0400

    Add files via upload

---
+++ Bitwise/hex/pt1/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+    int value = 9903;
+    
+    cout << "HEX OF 9903: ";
+    cout << std::hex << value << endl;
+
+
+    cout << std::hex << 0xC3 << endl;
+}
