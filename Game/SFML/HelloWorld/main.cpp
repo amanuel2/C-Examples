3877a58130efa2dd598e4c15c15cd71b1284ab06
Amanuel <amanuelb43@gmail.com>
2016-05-20 13:25:52 -0400

    Create main.cpp

---
+++ Game/SFML/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+#include<windows.h>
+
+using std::cout;
+using std::endl;
+int main()
+{
+    sf::RenderWindow _WINDOW(sf::VideoMode(600,600), "HELLO WORLD");
+
+    while(_WINDOW.isOpen() )
+    {
+    
+    }
+}
