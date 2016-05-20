8e88a77a64433e45e4684fd6f2694bc9636b4e2b
Amanuel <amanuelb43@gmail.com>
2016-05-20 13:58:34 -0400

    Create run.sh

---
+++ Game/SFML/Event/Binary-Project/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+###############               ############
+############### ONLY ON LINUX ############
+###############               ############
+#Tested on Ubuntu Distro.
+echo "_________________________"
+echo "|                       |"
+echo "|Author : Amanuel Bogale|"
+echo "|                       |"
+echo "-------------------------"
+echo "Building C++ File."
+
+g++ main.cpp -o main.out -pthread -std=c++11
+
+echo "Compiling C++ File."
+
+./main.out
+
