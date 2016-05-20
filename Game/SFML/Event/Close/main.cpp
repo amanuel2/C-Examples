7f0279663571e93bcba42c7fd72f3890e4ccbd8d
Amanuel <amanuelb43@gmail.com>
2016-05-20 13:26:43 -0400

    Create main.cpp

---
+++ Game/SFML/Event/Close/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+#include<windows.h>
+
+using std::cout;
+using std::endl;
+int main()
+{
+    sf::RenderWindow _WINDOW(sf::VideoMode(600,600), "HELLO WORLD");
+
+    while(_WINDOW.isOpen() )
+    {
+      sf::Event _EVENT;
+      while(_WINDOW.pollEvent(_EVENT))
+      {
+          switch(_EVENT.type)
+          {
+          case sf::Event::Closed:
+                _WINDOW.close();
+                Sleep(3);
+                exit(1);
+                break;
+            }
+      }
+
+    }
+}
